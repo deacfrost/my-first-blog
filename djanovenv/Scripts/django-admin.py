bbe87011979c0e3a2d7488a4e3836c82fb01408e
deacfrost <deacfrost@mail.ru>
2017-09-29 10:34:37 +0700

    Django, first commit

---
+++ djanovenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\lixdv.iskra\pycharmprojects\untitled\new_work\help\web\django\djanovenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
